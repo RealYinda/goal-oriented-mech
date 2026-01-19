a04b2ddd8b505030732913a9252517cdc2581f92
RealYindaWang <realvintawang@gmail.com>
2026-01-19 10:55:32 +0800

    ErrorEst function added

--- ThermalSolver/build-source-Desktop-Default/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/Dev/Code_workspace/2026_TVLSI_Mech_AMR/LinearElasticSolver/source
-# Build directory: /home/Dev/Code_workspace/2026_TVLSI_Mech_AMR/ThermalSolver/build-source-Desktop-Default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
